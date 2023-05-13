de35bc3a30867a15029fa7e2e15bf5e666f3da72
Nishovec <shaymardanov_E@inbox.ru>
2023-05-13 11:48:35 +0600

    a

--- MyHashTable.java
+++ MyHashTable.java
@@ -19,8 +19,14 @@ public class MyHashTable<K, V> {
         chainArray = new HashNode[M];
         size = 0;
     }
-    private int hash(K key) {}
-    private void put(K key, V value) {}
+    private int hash(K key) {
+        return (key.hashCode() & 0*7ffffffff) %M;
+    }
+    private void put(K key, V value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("key or value cannot be null")
+        }
+    }
     public V get(K key) {}
     public V remove (K key) {}
     public boolean contains(V value) {}
