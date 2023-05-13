2d182907edd0cf5e4a077df733eecbcf5d38a94a
Nishovec <shaymardanov_E@inbox.ru>
2023-05-13 11:46:27 +0600

    a

---
+++ HashTable.java
@@ -0,0 +1,13 @@
+public class HashTable<K, V> {
+    private K key;
+    private V value;
+    private HashNode<K, V> next;
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "{" + key + " " + value + "}";
+    }
+}
--- MyHashTable.java
+++ MyHashTable.java
@@ -1,2 +1,28 @@
-public class MyHashTable {
+import java.util.Random;
+public class MyHashTable<K, V> {
+    private int x;
+    private String y;
+    private class HashNode<K, V >(int x, String y) {
+        this.x = x;
+        this.y = y;
+    }
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+    private int hash(K key) {}
+    private void put(K key, V value) {}
+    public V get(K key) {}
+    public V remove (K key) {}
+    public boolean contains(V value) {}
+    public K getkey(V value) {}
 }
